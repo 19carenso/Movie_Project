c50e88116859214e613dce49edf4646739aa1bcf
trasen1003 <tristan.gaudron@neuf.fr>
2020-10-19 15:11:02 +0200

    smoke velocity

--- NavMesh/Assets/ressources/MoveToBathtub.cs
+++ NavMesh/Assets/ressources/MoveToBathtub.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.AI;
+using UnityEngine.VFX;
 
 public class MoveToBathtub : MonoBehaviour
 //==========================================================================
@@ -10,8 +11,11 @@ public class MoveToBathtub : MonoBehaviour
 // first version (from Sykoo)
 {
 	private NavMeshAgent agent;
+	private Vector3 last_position;
+	// we keep track of the agent's position to get the velocity when we it's needed
     private Transform bathtub;
 	private bool isBoosted;
+	private	VisualEffect smoke_vfx;
 	static public float timeBoost;
 	
 
@@ -19,15 +23,20 @@ public class MoveToBathtub : MonoBehaviour
 	{
 		agent = GetComponent<NavMeshAgent>(); // The agent is a component of the same GameObject (than this script)
 		
+		smoke_vfx = GetComponent<Transform>().Find("smoke").gameObject.GetComponent<VisualEffect>(); //Vfx component to access to the shader's options
 		bathtub = GameObject.Find("Bathtub").transform;
 		isBoosted = false;
 		agent.destination = bathtub.position;
-		
+
 	}
 
 	void Update()
 	{
-		if(!isBoosted)
+		Vector3 velocity = agent.transform.position - last_position;
+
+		smoke_vfx.SetVector3("speed", velocity*100); //Update of the smoke's speed based on the shadow's destination
+
+		if (!isBoosted)
         {
 			agent.speed = GenerateEnemies.agentSpeed;
 			agent.acceleration = GenerateEnemies.agentAcceleration;
@@ -43,8 +52,9 @@ public class MoveToBathtub : MonoBehaviour
         {
 			agent.isStopped = true;
         }
-		
-		
 
+
+
+		last_position = agent.transform.position;
 	}
 }
