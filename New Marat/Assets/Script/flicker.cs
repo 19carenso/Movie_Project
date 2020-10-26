fc619eb121b7ba69ff2dce10106d5a7506aa8f53
trasen1003 <tristan.gaudron@neuf.fr>
2020-10-26 14:12:05 +0100

    candle integrated in the scene and candle diseappearing based on speed

---
+++ New Marat/Assets/Script/flicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flicker : MonoBehaviour
+{
+    public float intensity_prop;
+    public float freq;
+    public float random_offset;
+
+    private float intensity_init;
+    private float time;
+    private Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        intensity_init = light.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime*(1 - random_offset * Random.Range(-random_offset, random_offset));
+        light.intensity = intensity_init + intensity_init*intensity_prop*Mathf.Sin(time*2*Mathf.PI*freq);
+    }
+}
