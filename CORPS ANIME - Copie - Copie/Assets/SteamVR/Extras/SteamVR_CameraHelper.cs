e5e0fad0399a6a681f9ec7925782396c8c4d64bc
trasen1003 <tristan.gaudron@neuf.fr>
2020-11-04 14:49:17 +0100

    à toi maxime

---
+++ CORPS ANIME - Copie - Copie/Assets/SteamVR/Extras/SteamVR_CameraHelper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Valve.VR
+{
+	public class SteamVR_CameraHelper : MonoBehaviour
+	{
+		void Start()
+		{
+			Debug.Log("Transform Tag is: " + gameObject.tag);
+#if OPENVR_XR_API && UNITY_LEGACY_INPUT_HELPERS
+			if (this.gameObject.GetComponent<UnityEngine.SpatialTracking.TrackedPoseDriver>() == null)
+			{
+				this.gameObject.AddComponent<UnityEngine.SpatialTracking.TrackedPoseDriver>();
+			}
+#endif
+		}
+	}
+}
